d68633316cddc05a90249f6687680afe212d09c8
denzel cheung <denzelcheung@shaw.ca>
2019-03-02 15:04:12 -0800

    testing

--- app/main.py
+++ app/main.py
@@ -94,13 +94,13 @@ def move():
     snake = getSnakeInfo(data)
     head_x = snake['head']['x']
     head_y = snake['head']['y']
-    if board[head_x+1][head_y] is 0 or 5:
+    if (board[head_x+1][head_y] is 0 or 5) and (board[head_x+1][head_y] < data['board']['width']):
         return move_response(directions[3])
-    elif board[head_x-1][head_y] is 0 or 5:
+    elif (board[head_x-1][head_y] is 0 or 5) and head_x-1 > 0:
         return move_response(directions[1])
-    elif board[head_x][head_y+1] is 0 or 5:
+    elif (board[head_x][head_y+1] is 0 or 5) and (board[head_x][head_y+1] < data['board']['height']):
         return move_response(directions[2])
-    elif board[head_x][head_y-1] is 0 or 5:
+    elif (board[head_x][head_y-1] is 0 or 5) and head_y-1 > 0:
         return move_response(directions[0])
 
 
